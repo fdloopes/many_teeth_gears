d76c2c41cc76ada4ad6e9bf5379eb7903d5987f9
fdloopes <fdloopes@gmail.com>
2021-07-06 11:50:56 -0300

    Adjusted prints

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Apr 27 16:25:43 2021
 
-Implementation of a program that uses image processing to detect 
+Implementation of a program that uses image processing to detect
 how many teeth the gears have.
 
 @author: fdlopes
@@ -46,15 +46,15 @@ for c in cnts:
     cY = int(M["m01"] / M["m00"])
     # Take the radius of the circle that covers the image.
     _ ,radius = cv2.minEnclosingCircle(c)
-    # Paint the center of the piece white using the radius of the circle that 
-    # covers the piece. 
-    # The -20 is the number of pixels you must shrink the circle to be able to 
+    # Paint the center of the piece white using the radius of the circle that
+    # covers the piece.
+    # The -20 is the number of pixels you must shrink the circle to be able to
     # capture the separate teeth of the piece.
     cv2.circle(img, (cX, cY), (int(radius)-20), (255, 255, 255), -1)
     # Show the image
     cv2.imshow("Image with center removed",img)
 
-# Smooths the image.    
+# Smooths the image.
 smooth = cv2.blur(img, (9, 9))
 # Transforms to grayscale.
 gray = cv2.cvtColor(smooth, cv2.COLOR_BGR2GRAY)
@@ -62,11 +62,12 @@ cv2.imshow("Grayscale image with smoothing and white center",gray)
 
 # Binarize the image
 (t,binInverse) = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY_INV)
-cv2.imshow("Binary Inverse. Final result",binInverse)
 
 # Find all contours in the image
 contours, _ = cv2.findContours(binInverse, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # Displays the number of contours found, referring to the number of teeth found.
 print("Number of teeth: "+str(len(contours)))
+cv2.putText(binInverse,"Number of teeth: "+str(len(contours)),(0,binInverse.shape[0]),cv2.FONT_HERSHEY_DUPLEX,1,255)
+cv2.imshow("Binary Inverse. Final result",binInverse)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
